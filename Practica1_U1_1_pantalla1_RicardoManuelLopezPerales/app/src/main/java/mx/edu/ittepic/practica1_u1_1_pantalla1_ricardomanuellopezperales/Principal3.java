d2fe7de903650a689b04514ac9d67f0a88a209dc
unknown <rimalopezpe@ittepic.edu.mx>
2018-10-26 20:34:51 -0600

    Hola a todos

---
+++ Practica1_U1_1_pantalla1_RicardoManuelLopezPerales/app/src/main/java/mx/edu/ittepic/practica1_u1_1_pantalla1_ricardomanuellopezperales/Principal3.java
@@ -0,0 +1,13 @@
+package mx.edu.ittepic.practica1_u1_1_pantalla1_ricardomanuellopezperales;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Principal3 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_principal3);
+    }
+}
